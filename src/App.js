e87bf8e02b634ececb977eb5138f72e9d713119d
diniebee <diniezubir@gmail.com>
2024-11-26 05:02:08 +0800

    Fixed logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import React from 'react';
+
 import './App.css';
+import logo from "./eggyheart_logo.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="../eggyheart_logo.png" alt='prop'/>
+        <img src={logo} alt="logo"/>
         <h1>Heyy</h1>
       </header>
     </div>
